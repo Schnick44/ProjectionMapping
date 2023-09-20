dcee19be1ae0ee6241a0b01f1fd3f6ef34a3fe5f
Nick Schneider <nick.schneider@esveo.com>
2023-09-20 19:53:22 +0200

    create textmesh component for enitites

--- Assets/Scripts/OnHover.cs
+++ Assets/Scripts/OnHover.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
-public class HoverManager : MonoBehaviour
+public class OnHover : MonoBehaviour
 {
     float hover_opacity = 0.5f;
     MeshRenderer m_Renderer;
+    Color m_Color;
+
+    private GameObject TextGameObject;
+    private TextMesh m_TextMesh;
+
+    public Vector3 DisplayLocation = new Vector3(0, 0, 0);
+    public Vector3 DisplayRotation = new Vector3(0, 0, 0);
+
+    private const string label = "The count is:";
+    private int i = 0;
 
     void Start()
     {
         m_Renderer = GetComponent<MeshRenderer>();
+        m_Color = m_Renderer.material.color;
+
+        TextGameObject = new GameObject();
+        TextGameObject.name = "TextGameObject";
+        m_TextMesh = TextGameObject.AddComponent(typeof(TextMesh)) as TextMesh;
+        m_TextMesh.text = "init";
     }
     void OnMouseOver()
     {
-        m_Renderer.material.color.a = hover_opacity;
+        Debug.Log("over");
+
+        m_Color.a = hover_opacity;
+
+        m_TextMesh.text = label + i;
+        i++;
     }
 
     void OnMouseExit()
     {
-        m_Renderer.material.color.a = 1f;
+        Debug.Log("exit");
+        m_Color.a = 1f;
+
+        m_TextMesh.text = "";
     }
 }
