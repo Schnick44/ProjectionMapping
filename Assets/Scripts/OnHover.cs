3875cade07fa65723dc7f304357a55fd6aea0804
nosc364d <nora.schaefer@mailbox.tu-dresden.de>
2023-09-24 19:45:20 +0200

    added transparency effect to hover script

--- Assets/Scripts/OnHover.cs
+++ Assets/Scripts/OnHover.cs
@@ -10,6 +10,11 @@ public class OnHover : MonoBehaviour
     MeshRenderer m_Renderer;
     Color m_Color;
 
+    // material of object, that script is attached to
+    private Material m_Material;
+    private Color m_MaterialColor;
+
+
     public TMP_Text WorldText;
 
     private const string label = "The count is:";
@@ -19,18 +24,32 @@ public class OnHover : MonoBehaviour
     {
         m_Renderer = GetComponent<MeshRenderer>();
         m_Color = m_Renderer.material.color;
-
+        // very hacky to be able to manipulate alpha values
+        m_Material = GetComponent<Renderer>().material;
+        m_MaterialColor = m_Material.color;
     }
     void OnMouseOver()
     {
         m_Color.a = hover_opacity;
         WorldText.text = label + i;
         i++;
+        // check for transparency and adjust if necessary
+        if (m_MaterialColor.a > hover_opacity) 
+        {
+            m_MaterialColor.a = hover_opacity;
+            m_Material.color = m_MaterialColor;
+        }
     }
 
     void OnMouseExit()
     {
         WorldText.text = "";
         m_Color.a = 1f;
+        // check transparency + adjust
+        if (m_MaterialColor.a < 1f) 
+        {
+            m_MaterialColor.a = 1f;
+            m_Material.color = m_MaterialColor;
+        }
     }
 }
